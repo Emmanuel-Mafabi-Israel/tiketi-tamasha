6fe9e27a66ddca492a179ed83586b5e5f99516dc
Israel-Mafabi-Emmanuel <israelmaleza01@outlook.com>
2025-02-18 07:15:02 +0300

    mafabi: server setup, model creation, static - templates creation.

--- server/app/config.py
+++ server/app/config.py
@@ -9,7 +9,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')  # for us it will be 'sqlite:///tiketi_tamasha.db'
+    SECRET_KEY                     = os.getenv('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI        = os.getenv('SQLALCHEMY_DATABASE_URI')  # for us it will be 'sqlite:///tiketi_tamasha.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False  # Suppress warnings
-    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    JWT_SECRET_KEY                 = os.getenv('JWT_SECRET_KEY')
