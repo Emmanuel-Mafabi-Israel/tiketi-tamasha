d4a531a486251662a4617598a51a1ebcf7190dae
Israel-Mafabi-Emmanuel <israelmaleza01@outlook.com>
2025-02-26 11:16:11 +0300

    TTD-13: [mafabi] retrieved updates from ali's branch.

--- client/tiketi-tamasha-client/src/App.js
+++ client/tiketi-tamasha-client/src/App.js
@@ -1,20 +1,24 @@
-import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-import AppRoutes from "./routes";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import { AuthProvider } from "./context/AuthContext";
 
-const App = () => {
+
+function App() {
   return (
-    <AuthProvider>
-      <Router>
-        <Navbar />
-        <AppRoutes />
-        <Footer />
-      </Router>
-    </AuthProvider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-};
+}
 
 export default App;
